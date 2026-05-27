b91846a7b154bd49b13283e138621108c4d8503d
JoyIsHappii <chayaninkosol@gmail.com>
2026-05-27 15:41:10 -0700

    done step 3

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import HTTP from 'http';
+
+const server = HTTP.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
